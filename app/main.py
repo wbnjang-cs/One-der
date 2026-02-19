e5883d06264ff68fa95f08efe26a9a37ace43422
Woobin Jang <wbnjang@gmail.com>
2026-02-18 23:27:27 -0800

    Just learning fastapi

--- app/main.py
+++ app/main.py
@@ -1,2 +1,23 @@
+from fastapi import FastAPI, HTTPException
 
-print("hello world")
+app = FastAPI()
+
+items = []
+
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items/{item_id}")
+def get_item(item_id:  int) -> str:
+     if item_id < len(items):
+        item = items[item_id]
+        return item
+     else:
+         raise HTTPException(status_code=404, detail = f"Item {item_id} not found")
