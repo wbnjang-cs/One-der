e2f9a4ac369ccea3acf975ee204ef893c5a11292
Woobin Jang <wbnjang@gmail.com>
2026-02-22 20:57:57 -0800

    basic outline, next work on implementng datetime

--- app/main.py
+++ app/main.py
@@ -1,23 +1,33 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import datetime
+from uuid import uuid4
 
 app = FastAPI()
+class Event(BaseModel):
+    name: str = "myEvent"
+    year: int
+    month: int
+    day: int
+    is_done: bool = False
 
-items = []
+    def __str__(self):
+        return f"{self.name} is scheduled for {self.year}/{self.month}/{self.day}."
 
+events = {}
 
 @app.get("/")
-def root():
-    return {"Hello": "World"}
+def get_events():
+    return events
 
-@app.post("/items")
-def create_item(item: str):
-    items.append(item)
-    return items
+@app.post("/events")
+def create_event(event: Event):
+    events[str(uuid4())] = event
+    if(event.month > 12 or event.month < 1):
+        raise HTTPException(status_code=406, detail = "Entered month is invalid")
+    if(event.day > 32 or event.day < 1):
+        raise HTTPException(status_code=406, detail = "Entered day is invalid")
+        
 
-@app.get("/items/{item_id}")
-def get_item(item_id:  int) -> str:
-     if item_id < len(items):
-        item = items[item_id]
-        return item
-     else:
-         raise HTTPException(status_code=404, detail = f"Item {item_id} not found")
+    return {"message" : f"{event}, "}
+    
